bd7d13a532a0d41380e09e62863d69d07ac50e31
edg <122878019+sadhubby@users.noreply.github.com>
2025-05-12 23:32:36 +0800

    feat: added command functions
    Also added stylization (printing text with colors)

--- CSOPESY_C.c
+++ CSOPESY_C.c
@@ -5,20 +5,77 @@
 #include <stdlib.h>
 // #include <threads.h> check documentations how to put this in cuz not work rn  
 
+// Colors use for design
+#define yellow "\x1b[33m"
+#define green "\x1b[32m"
+#define reset "\x1b[0m"
+
+// Prototype so as to only see main at top
+void header();
+void initialize();
+void screen();
+void scheduler_test();
+void scheduler_stop();
+void report_util();
+void clear();
+
+// design decision
+void print_color(const char *color, const char *text);
+
 int main(){
+    char str[100];
+    header();
+    printf("");
+    print_color(green, "Hello! Welcome to PEFE-OS command line!\n");
+    print_color(yellow, "Type 'exit' to quite, 'clear' to clear the screen\n");
+
+    printf("Enter command: \n");
+    fgets(str, sizeof(str), stdin);
+
+    printf("%s", str);
+
+
+    
+    return 0;
+}
+
+void header(){
+
     FILE *file = fopen("ascii.txt", "r");
     if(file == NULL){
         perror("Error opening file");
-        return 1;
     }
 
     char line[256];
     while(fgets(line, sizeof(line), file)){
         printf("%s", line);
     }
-
     fclose(file);
-    return 0;
-
 
 }
+
+void print_color(const char *color, const char *text){
+    printf("%s%s%s", color, text, reset);
+}
+
+void initialize(){
+    printf(" command recognized. Doing something");
+}
+
+void screen(){
+    printf(" command recognized. Doing something");
+}
+
+void scheduler_test(){
+    printf(" command recognized. Doing something");
+}
+
+void scheduler_stop(){
+    printf(" command recognized. Doing something");
+}
+
+void report_util(){
+    printf(" command recognized. Doing something");
+}
+
+void clear();
