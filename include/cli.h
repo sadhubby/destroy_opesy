8de6f7dd18ce39d43cec3696209816113e718aeb
pannkakkes <fiona.amaia@gmail.com>
2025-06-27 15:57:34 +0800

    Add process.h

--- include/cli.h
+++ include/cli.h
@@ -1,8 +1,6 @@
 #ifndef CLI_H
 #define CLI_H
 
-//#include "config.h" - to be added
-
 void runCLI();
 
 #endif
---
+++ include/process.h
@@ -0,0 +1,26 @@
+#include <ctime>
+
+#ifndef PROCESS_H
+#define PROCESS_H
+
+typedef struct{
+
+    char name[32];
+    int total_prints;
+    int finished_print;
+
+    time_t start_time;
+    time_t end_time;
+    int core_assigned;
+
+    int burst_time;
+    int is_finished;
+
+    char filename[64];
+
+} Process;
+
+Process *create_process(const char *name, int pid);
+void destroy_process(Process *p);
+
+#endif
