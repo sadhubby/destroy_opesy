a197081b00819f05666c86875948122752fd53cd
pannkakkes <fiona.amaia@gmail.com>
2025-07-04 18:55:47 +0800

    Redefine process struct

--- include/process.h
+++ include/process.h
@@ -1,21 +1,71 @@
-#include <time.h>
-#include "instruction.h"
-
 #ifndef PROCESS_H
 #define PROCESS_H
 
+#include <stdint.h>
+#include <stdbool.h>
+
+#define MAX_PROCESS_NAME 50
+
+typedef enum {
+    READY,
+    RUNNING,
+    SLEEPING,
+    FINISHED
+} ProcessState;
+
+typedef struct {
+    char name[MAX_PROCESS_NAME];
+    uint16_t value;
+} Variable;
+
+typedef enum {
+    DECLARE,
+    ADD,
+    SUBTRACT,
+    PRINT,
+    SLEEP,
+    FOR_START,
+    FOR_END
+} InstructionType;
+
+typedef struct {
+    InstructionType type;
+    char arg1[50];
+    char arg2[50];
+    char arg3[50];
+    uint16_t value;
+    uint8_t repeat_count; 
+} Instruction;
+
+typedef struct {
+    char message[50];
+    int tick;
+} Log;
+
 typedef struct{
 
-    char name[50];
+    char name[MAX_PROCESS_NAME];
     int pid;
-    int total_prints;
-    int finished_print;
-    int is_finished;
+    ProcessState state;
+    int program_counter;
+    uint16_t sleep_until_tick;
+
+    Variable *variables;
+    int num_var;
+
     Instruction *instructions;
+    int num_inst;
+
+    Log *logs;
+    int num_logs;
+
+    int start;
+    int end;
+
+    bool is_in_scren;
 
 } Process;
 
 Process *create_process(const char *name, int pid);
-void destroy_process(Process *p);
 
 #endif
