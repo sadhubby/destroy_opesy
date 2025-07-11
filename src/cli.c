f551441c783519b1807da62a039a292a92aa59b7
pannkakkes <fiona.amaia@gmail.com>
2025-07-11 11:09:09 +0800

    Update cli.c

--- src/cli.c
+++ src/cli.c
@@ -74,6 +74,7 @@ void runCLI() {
         // scheduler-start
         else if (strcmp(command, "scheduler-start") == 0) {
             Process *dummy = generate_dummy_process(config.batch_process_freq);
+            print_process_info(dummy);
             add_process(dummy);
             init_ready_queue();
             enqueue_ready(dummy);
