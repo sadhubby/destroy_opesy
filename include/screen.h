bbf5561adb1567f3d53999142f1e504297743b66
sadhubby <evanm.deguzman@gmail.com>
2025-08-03 22:46:49 +0800

    feat: create a new screen process with memory in power of 2's

--- include/screen.h
+++ include/screen.h
@@ -1,7 +1,9 @@
 #ifndef SCREEN_H
 #define SCREEN_H
 
+#include <stdbool.h>
 #include "process.h"
+
 
 typedef struct{
     char name[50];
@@ -12,9 +14,9 @@ typedef struct{
 } ScreenSession;
 
 
-void screen_start(const char *name);
+void screen_start(const char *name, int memory_size);
 void screen_resume(const char *name);
 void screen_list(int num_cores, Process **cpu_cores, int finished_count, Process **finished_processes);
 void screen_create_with_code(const char *command_args);
-
+bool is_valid_memory_size(int memory_size);
 #endif
